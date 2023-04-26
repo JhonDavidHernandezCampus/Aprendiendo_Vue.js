889154cc4242964c866cc96053273aa36f5adcfb
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-04-25 19:02:23 -0500

    feat: :tada: Comienzo al proyecto y su documentacion pra prender vue

---
+++ main.js
@@ -0,0 +1,35 @@
+
+const app = Vue.createApp({
+    data(){
+        return{
+            message: 'Hollo Vue'
+        }
+    }
+}).mount('#app')
+
+
+/* const app = new Vue({
+    data(){
+        return {
+            message: 'Hello Vue'
+        }
+    }
+}).mount('# app')
+
+
+
+ */
+/* const app = new Vue({
+    /* aqui debe ir lo que vallamos a trabajar con Vue 
+    el: '#app',
+    data() {
+        return {
+            nombre:'Jhon hernandez'
+        }
+    },
+
+    
+})
+app.mount('#app')
+
+console.log(data()); */
